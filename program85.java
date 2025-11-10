6997c8329cf098d60253aa710c2f5d47bbd2aecc
Shradha Taskar <taskarshradha@gmail.com>
2025-11-10 13:14:03 +0530

    Add files via upload

---
+++ program85.java
@@ -0,0 +1,59 @@
+//  Perfect number reverse final code 
+import java.util.*;
+
+class Number
+{
+    public boolean CheckPerfect(int iNo)
+    {
+        int i = 0, iSum = 0;
+
+        if(iNo < 0)
+        {
+            return true;
+        }
+
+        // reverse loop
+        for(i = (iNo / 2) ; (i >= 1) && (iSum < iNo) ; i--)
+
+        {
+            if((iNo % i) == 0)
+            {
+                iSum = iSum + i;
+            }
+        }
+        return(iSum == iNo);  
+    }
+}    // End of Number class
+
+class program85
+{
+    public static void main (String A[])
+    {
+        int iValue = 0;
+        boolean bRet = false;
+      
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Number : ");
+        iValue = sobj.nextInt();
+
+        Number nobj = new Number();
+        bRet = nobj.CheckPerfect(iValue);
+        
+        if(bRet == true )
+        {
+            System.out.println(iValue + " is Perfect number");
+        }
+        else 
+        {
+            System.out.println(iValue + " is not a perfect number");
+        }
+
+        //IMPORTANT
+        sobj = null;
+        nobj = null;
+
+        System.gc();
+        
+    }
+}
